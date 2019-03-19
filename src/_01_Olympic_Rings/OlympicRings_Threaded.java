a7b44f46464c943edd3d7103a0609fa3ebb7ee40
ilabstaff <ilabstaff@172.16.116.68>
2019-03-18 17:56:16 -0700

    Going to work on Level 5 mod 1 Checkpoint

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,44 @@
 package _01_Olympic_Rings;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-
+	public static void main(String[] args) {
+		Robot[] robots = new Robot[5];
+		for(int i = 0; i < robots.length; i++) {
+			robots[i] = new Robot();
+			robots[i].setSpeed(5);
+		}
+		
+		Thread r1 = new Thread (()->drawCircle(Color.BLUE, robots[0], 100,100));
+		Thread r2 = new Thread (()->drawCircle(Color.YELLOW, robots[1], 200,300));
+		Thread r3 = new Thread (()->drawCircle(Color.BLACK, robots[2], 300,100));
+		Thread r4 = new Thread (()->drawCircle(Color.GREEN, robots[3], 400,300));
+		Thread r5 = new Thread (()->drawCircle(Color.RED, robots[4], 500,100));
+		
+		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+		r5.start();
+		
+	}
+	
+	static void drawCircle(Color c, Robot rob, int x, int y) {
+		rob.setX(x);
+		rob.setY(y);
+		rob.penDown();
+		rob.setPenColor(c);
+		for(int i=0; i < 360; i ++) {
+			rob.move(2);
+			rob.turn(1);
+		}
+		rob.penUp();
+		rob.setX(500);
+		rob.setY(500);
+	}
 }
 
--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,35 @@
 package _04_Thread_Pool;
 
-public class ThreadPool {
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+public class ThreadPool {
+	private Thread[] threads;
+	private ConcurrentLinkedQueue<Task> taskQueue;
+	
+	public ThreadPool(int totalThreads){
+		threads = new Thread[totalThreads];
+		taskQueue = new ConcurrentLinkedQueue<Task>();
+		for (int i = 0; i < threads.length; i++) {
+			threads[i] = new Thread(new Worker(taskQueue));
+		}
+		
+	}
+	
+	public void addTask(Task task) {
+		taskQueue.add(task);
+	}
+	
+	public void start() {
+		for (Thread task : threads) {
+			task.start();
+		}
+		for (Thread task : threads) {
+			try {
+				task.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
